23daa39d592a11acf88ef00e17f32ee7262a6696
Craig <craigjope@gmail.com>
2022-02-19 16:16:41 -0700

    User
    User Resource Added

---
+++ db/migrate/20220219231602_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :img_url
+      t.string :bio
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
