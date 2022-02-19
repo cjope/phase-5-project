bf65dad8b9837b3f40b3d5bfcd788004f3d8bb46
Craig <craigjope@gmail.com>
2022-02-19 16:59:11 -0700

    User
    Controller: methods added, rescue_from added. Model: macros validation added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,47 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_not_found
+
+    def index
+        render json: User.all
+      end
+    
+      def show
+        user = User.find_by(id:)
+        render json: user
+      end
+    
+      def create
+        user = User.create!(user_params)
+        session[:user_id] = user.id
+        render json: user, status: :created
+      rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: invalid.record.errors.full_messages.first }, status: :unprocessable_entity
+      end
+    
+      def update
+        user = User.find_by(id:)
+        user.update!(user_params)
+        render json: user, status: :ok
+      end
+    
+      def destroy
+        user = User.find_by(id:)
+        user.destroy!
+        head :no_content
+      end 
+    
+      private
+    
+        def user_params
+          params.permit(:username, :first_name, :last_name, :password, :password_confirmation, :img_url, :age, :bio)
+        end
+    
+        def user_update_params
+          params.permit(:username, :img_url)
+        end
+
+        def render_not_found(invalid)
+            render json: { errors: invalid.record.errrors.full_messages}
+        end
+    
 end
