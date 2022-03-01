5b1fb4e4349af98b63ec5c15297d6f8b43d5dd61
Craig <craigjope@gmail.com>
2022-03-01 09:53:03 -0700

    user.rb case
    Create before action for case sensitivity

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
 
     validates :username, presence: true, uniqueness: true
 
+    before_create :case_names
+
+    def case_names
+        self.first_name = first_name.capitalize
+        self.last_name = last_name.capitalize
+        self.username = username.downcase
+        self.email = email.downcase
+    end
+
 end
