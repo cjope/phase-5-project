050a7e2734949601f40d23f9fad33812c46199ec
Craig <craigjope@gmail.com>
2022-03-01 08:55:58 -0700

    route /me

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/me/:id", to: "users#show"
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
 end
