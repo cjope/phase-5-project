8cc553e122a92b8b7fb605208158ec4de68fdcf8
Craig <craigjope@gmail.com>
2022-03-01 09:33:18 -0700

    App.js updated
    fetch/view test user information

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import User from "./User";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/me/1").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      } else {
+        return r.json().then((user) => console.log(user.error));
+      }
+    });
+  }, []);
+
+  return <User user={user} />;
 }
 
 export default App;
